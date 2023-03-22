42297f0da0ca00cdae351a0b7ad0eebbfa0aef28
Kristina Orolić <kristina.orolic@ewasoft.rs>
2023-03-22 21:35:36 +0100

    Add project files.

---
+++ FirstStatelessWebClient/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FirstStatelessWebClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
